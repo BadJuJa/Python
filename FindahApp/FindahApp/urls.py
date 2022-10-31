51a11d3882ce5d018d0e1101e27babe06b16e135
Евгений <jamescloudeve@gmail.com>
2022-11-01 02:33:23 +0300

    _

--- FindahApp/FindahApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from simFindah.views import search_view, search_result
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', search_view, name = 'home'),
-path('search_result/', search_result, name = 'search-result')
-]
