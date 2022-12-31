e2ff99ca01b962eac041358f6ae3d64723915245
Евгений <jamescloudeve@gmail.com>
2023-01-01 01:06:29 +0300

    deleted the garbage

--- NSFWRecognition/main.py
+++
@@ -1,3 +0,0 @@
-import os
-
-print(os.path.join("path1", "path2", "path3"))
--- TelegramBot/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-import telebot
-
-URL = 'https://randomall.ru/api/general/unexpected_event'
-x = requests.post(URL)
-
-print(x.text)
